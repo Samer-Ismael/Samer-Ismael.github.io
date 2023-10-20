06fe21e6a77803d803b83ed8ed419fca0790d3b5
Samer-Ismael <samerismaeelbat2@hotmail.com>
2023-10-20 04:08:35 +0200

    Saving

---
+++ script.js
@@ -0,0 +1,29 @@
+function animateProgressBars() {
+  const progressBars = document.querySelectorAll('.progress-bar');
+
+  progressBars.forEach((bar) => {
+    const targetPercentage = parseInt(bar.style.width);
+    let currentPercentage = 0;
+    const interval = setInterval(() => {
+      if (currentPercentage < targetPercentage) {
+        currentPercentage++;
+        bar.style.width = `${currentPercentage}%`;
+        bar.textContent = `${currentPercentage}%`;
+      } else {
+        clearInterval(interval);
+      }
+    }, 10); // Adjust the animation speed
+  });
+}
+
+// Call the animation function when the page is loaded
+window.addEventListener('load', animateProgressBars);
+
+function toggleDropdown(id) {
+  const dropdownContent = document.getElementById(id + '-content');
+  if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none';
+  } else {
+    dropdownContent.style.display = 'block';
+  }
+}
